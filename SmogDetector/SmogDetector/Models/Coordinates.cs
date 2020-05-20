a87afb1a8b4d143ea6d21997542b8009981dbfc1
KonraddGit <konradparchanski@gmail.com>
2020-05-20 18:05:47 +0200

    PassVariables

---
+++ SmogDetector/SmogDetector/Models/Coordinates.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SmogDetector.Models
+{
+    public class Coordinates
+    {
+        public double Lat { get; set; }
+        public double Lng { get; set; }
+        public string AirPollution { get; set; }
+    }
+}
