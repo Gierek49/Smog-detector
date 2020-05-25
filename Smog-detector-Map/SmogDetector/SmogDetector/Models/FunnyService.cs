19c450a1c4eeb23a41d1427bd365a62c83a5241b
Fluniks <49128004+Fluniks@users.noreply.github.com>
2020-05-25 19:33:15 +0200

    dodatkowe zmiany

--- Smog-detector-Map/SmogDetector/SmogDetector/Models/FunnyService.cs
+++ Smog-detector-Map/SmogDetector/SmogDetector/Models/FunnyService.cs
@@ -8,18 +8,18 @@ using RestSharp;
 namespace Giphy
 {
     public class GpihyServices
-    {
+    { 
         private RestClient client =
             new RestClient("http://api.giphy.com/v1/gifs");
         public string GetRandom(string tag)
         {
             var request = new RestRequest("random");
             request.AddQueryParameter("tag", tag);
-            request.AddQueryParameter("apiKey", "iqCe7luZhP7t1NEC0SXP4IASPmIl1XxV");
+            request.AddQueryParameter("apiKey", "WOIU0IQCdNukp06oImt9tPAFV5dDpsHW");
             var response = client.Execute<GiphyResponse>(request);
             if (response.IsSuccessful)
             {
-                return response.Data.Data.Images.Original.Url;
+               string x = response.Data.Data.Images.Original.Url;
             }
 
             return null;
